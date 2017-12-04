51f0c5a2abd74dfcc90a01724d4e5d3d480cb2aa
fenixlod <fenix_lod@abv.bg>
2017-12-04 02:21:43 +0200

    Fixed static resource servind and index.html serving

--- src/main/java/com/project/EsportSchedule/additional/Configuration.java
+++ src/main/java/com/project/EsportSchedule/additional/Configuration.java
@@ -5,15 +5,17 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 import com.project.EsportSchedule.data.ISportsDAO;
 import com.project.EsportSchedule.data.SportsDAO;
 
 @org.springframework.context.annotation.Configuration
 @ComponentScan(basePackages="com.project.EsportSchedule")
-@EnableWebMvc
+//@EnableWebMvc
 public class Configuration extends WebMvcConfigurerAdapter {
 	
 	@Bean
@@ -34,7 +36,7 @@ public class Configuration extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
-          .addResourceHandler("/files/**")
-          .addResourceLocations("file:/opt/files/");
+        	.addResourceHandler("/**")
+        	.addResourceLocations("file:src/main/resources/static/");
      }
 }
